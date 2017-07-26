d5ee6c65bda5e3166c4dcb41149a76448e4e7bf2
Mollie McIntyre <mollie.mcintyre9@gmail.com>
2017-07-25 18:54:40 -0700

    First commit

---
+++ lib/kele.rb
@@ -0,0 +1,12 @@
+'require HTTParty'
+
+class Kele
+	include HTTParty
+
+  def initialize(email, password)
+  	response = self.class.post("https://www.bloc.io/api/v1/sessions", body: {"email": email, "password": password})
+  	raise "Invalid Email or Password" if response.code != 200
+  	@auth_token = response["auth_token"]
+  end
+
+end
