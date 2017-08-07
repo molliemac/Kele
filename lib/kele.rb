2c6c069dc5f979664d0f3b8dbdfbb07dbc6d5029
Mollie McIntyre <mollie.mcintyre9@gmail.com>
2017-08-06 23:50:34 -0700

    added create_submission

--- lib/kele.rb
+++ lib/kele.rb
@@ -45,4 +45,17 @@ class Kele
   	puts response
   end
 
+  def create_submission (checkpoint_id, assignment_branch, assignment_commit_link, comment, enrollment_id)
+  	response = self.class.post("https://www.bloc.io/api/v1/checkpoint_submissions", 
+  		body: {
+  			checkpoint_id: checkpoint_id, 
+  			assignment_branch: assignment_branch,
+  			assignment_commit_link: assignment_commit_link,
+  			comment: comment,
+  			enrollment_id: enrollment_id
+  			}, 
+  			headers: { "authorization" => @auth_token } )
+  	puts response
+  end
+
 end
